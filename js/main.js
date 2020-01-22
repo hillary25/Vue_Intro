dd3a6187fe41f037d5dbc3e2c68b1b5d0cfc45ec
Hillary <h_strong69707@fanshaweonline.ca>
2020-01-22 09:51:34 -0500

    adding live data

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
+// To do: => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
 
 var vm = new Vue({
   el: "#app",
 
   data: {
 
-    // mock up the user - this well eventually come from the database UMS (user management system)
+    // Mock up the user - this well eventually come from the database UMS (user management system)
     user: {
-      isAdmin: false,
-      avatar: null, //"thor.png",
-      isLoggedIn: true
+      isLoggedIn: true,
+      settings: {}
     },
 
 
@@ -27,18 +26,39 @@ var vm = new Vue({
     showDetails: false
   },
 
+  created: function() {
+    // Run a fetch call and get the user data
+    console.log('created lifecycle hook fired here, go get user data');
+    this.getUserData();
+  },
+
   methods: {
+    getUserData() {
+      // Do a fetch call here and get the user from the DB
+      const url = './includes/index.php?getUser=1';
+
+      fetch(url) // Get data from the DB
+      .then(res => res.json()) // Translate JSON from DB to plain object
+      .then(data => { // Use the plain data object (the user)
+        console.log(data); // Log it to the console (testing)
+
+        // Put our DB data into Vue
+        this.user.settings = data[0];
+      })
+      .catch((error) => console.error(error))
+    },
+
     setUserPrefs() {
-      // this is the preferences control, hit the API when ready (or use a component)
+      // This is the preferences control, hit the API when ready (or use a component)
       console.log('set user prefs here');
     },
 
     userLogin() {
-      // call the login route, and / or load the login component
+      // Call the login route, and / or load the login component
       console.log('do login  / logout on click');
 
-      // this is a ternary statement -> shorthand for if / else
-      //the expression evaluates to true or false - if it's true, set the value equal to the left of the colon, if it's false set the value equal to the right
+      // This is a ternary statement -> shorthand for if / else
+      // The expression evaluates to true or false - if it's true, set the value equal to the left of the colon, if it's false set the value equal to the right
       this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
     },
 
@@ -49,7 +69,7 @@ var vm = new Vue({
       this.vidsource = vidsource;
       this.videodescription = description;
 
-      // make the movie details show up
+      // Make the movie details show up
       this.showDetails = true;
     }
   }
