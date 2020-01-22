4fa3d457696631508c76598044c22fa8bf80a8b7
Hillary <h_strong69707@fanshaweonline.ca>
2020-01-22 08:32:28 -0500

    added working files

--- js/main.js
+++ js/main.js
@@ -1,57 +1,55 @@
-// To do => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
 
 var vm = new Vue({
   el: "#app",
 
   data: {
 
-    // Mock up the user here - this will eventually come from the database UMS (user management system)
+    // mock up the user - this well eventually come from the database UMS (user management system)
     user: {
       isAdmin: false,
-      avatar: null, 
-      // Make avatar "thor.png" above if you want to see avatar picture
+      avatar: null, //"thor.png",
       isLoggedIn: true
     },
-    // Object properties need to be seperated by a comma, like above
 
 
-    // This data should come from a database, but we'll just mock it up for now
+    // this data would also come from the database, but we'll just mock it up for now
     videodata: [
       { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
       { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
       { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
     ],
 
-    videotitle: "video title goes here",
+    vidtitle: "video title goes here",
     vidsource: "",
-    videodescription: "video description goes here",
+    videodescription: "video description here",
 
     showDetails: false
   },
 
   methods: {
     setUserPrefs() {
-      // This is the preferences control, hit the api when ready (or use a component)
+      // this is the preferences control, hit the API when ready (or use a component)
       console.log('set user prefs here');
     },
 
     userLogin() {
-      // Call the login reoute, and / or load the login component
-      console.log('do login / logout on click');
+      // call the login route, and / or load the login component
+      console.log('do login  / logout on click');
 
-      // This is a ternary statement -> shorthand for if / else
-      // The expression evaluates to true or false - if it's true, set the value equal to the left of the colon. If it's false, set the value equal to the right.
+      // this is a ternary statement -> shorthand for if / else
+      //the expression evaluates to true or false - if it's true, set the value equal to the left of the colon, if it's false set the value equal to the right
       this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
     },
 
-    showMovieDetails({ name, vidsource, description }) {
-      // console.log('show these details: ', movie);
+    showMovieDetails({name, vidsource, description}) {
+      //console.log('show these details: ', movie);
 
-      this.videotitle = name;
+      this.vidtitle = name;
       this.vidsource = vidsource;
       this.videodescription = description;
 
-      // Make the movie details show up
+      // make the movie details show up
       this.showDetails = true;
     }
   }
